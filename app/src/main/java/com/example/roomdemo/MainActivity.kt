4354f949575018f5d9e3aa2034e08d16bfbe226c
A1exanderKol <mr.kolesov.kol@gmail.com>
2025-10-29 21:30:25 +0700

    add TitleRow

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.roomdemo.ui.theme.RoomDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +30,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TitleRow(head1: String, head2: String, head3: String) {
+    Row (
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(head1, color = Color.White,
+            modifier = Modifier
+                .weight(0.1f))
+        Text(head2, color = Color.White,
+            modifier = Modifier
+                .weight(0.2f))
+        Text(head3, color = Color.White,
+            modifier = Modifier.weight(0.2f))
     }
 }
 
